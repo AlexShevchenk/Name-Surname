0d6d83de70c0344977655bd214993e15c5ab78e6
Oleksandr Shevchenko <105636389+AlexShevchenk@users.noreply.github.com>
2023-03-02 18:42:36 +0200

    File about First Program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string firstName = "Олексанлр";
+        string lastName = "Шевченко";
+        int age = 23;
+        int yearsUntil40 = 40 - age;
+
+        Console.WriteLine($"Моє повне ім'я та прізвище: {firstName} {lastName}");
+        Console.WriteLine($"До початку нового життя залишилось {yearsUntil40} років");
+    }
+}
